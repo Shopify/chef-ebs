5e707256c752af6d2306eedb61e31f2d29048268
Jonathan Rudenberg <jonathan@titanous.com>
2012-07-18 14:25:40 -0400

    Add license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
-maintainer "Peritor GmbH"
-maintainer_email "scalarium@peritor.com"
+# Original source: https://github.com/scalarium/cookbooks/tree/master/ebs
+# Updated by Jonathan Rudenberg (jonathan@titanous.com)
+
+maintainer "Jonathan Rudenberg"
+maintainer_email "jonathan@titanous.com"
 description "Mounts attached EBS volumes"
-version "0.1"
+version "0.2"
 recipe "ebs::volumes", "Mounts attached EBS volumes"
 recipe "ebs::raids", "Mounts attached EBS RAIDs"
 
